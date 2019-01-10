d96d38f6dfe8e48cf7abba67b3cfa7409c159599
Gyarmati Ádám <gyarmatiaadaam@gmail.com>
2019-01-10 15:29:50 +0100

    feat(exceptions): handled

--- static/js/calculatorDom.js
+++ static/js/calculatorDom.js
@@ -4,9 +4,34 @@ const beer = document.getElementById('beer');
 const wine = document.getElementById('wine');
 const shot = document.getElementById('shot');
 const bloodAlcohol = document.getElementById('bloodalcohol');
+const form = document.getElementById('form');
+const submit = document.getElementById('submit');
+const result = document.getElementById('result');
+const goBack = document.getElementById('goback');
 
 //console.log(new Date().toLocaleString());
 
+submit.onclick = () => {
+  setNaNsToZeros();
+  setBloodAlcoholText();
+  result.classList.remove('hidden');
+  form.classList.add('hidden');
+};
+
+goBack.onclick = () => {
+  form.classList.remove('hidden');
+  result.classList.add('hidden');
+};
+
+const checkRequiredFields = () => {
+  if(isNaN(kg) || isNaN(time)) alert('Please provide time and weight!');
+};
+
+const setNaNsToZeros = () => {
+  if(isNaN(beer.value)) beer.value = 0;
+  if(isNaN(wine.value)) wine.value = 0;
+  if(isNaN(shot.value)) shot.value = 0;
+};
 
 const setBloodAlcoholText = () => {
   let grammBeer = beer.value * 4.5 * 0.8; 
