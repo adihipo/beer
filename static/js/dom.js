633925009722af478cc388aa3d0f75a968be8836
Gyarmati Ádám <gyarmatiaadaam@gmail.com>
2019-01-11 00:26:03 +0100

    style

--- static/js/dom.js
+++ static/js/dom.js
@@ -17,6 +17,9 @@ const goBack = document.getElementById('goback');
 const descriptionTitle = document.getElementById('descriptiontitle');
 const description = document.getElementById('description');
 const sober = document.getElementById('sober');
+const radios = document.getElementsByName('sex');
+const femaleLabel = document.getElementById('femalelabel');
+const maleLabel = document.getElementById('malelabel');
 
 var timer = 0;
 
@@ -24,11 +27,64 @@ beer.value = 0;
 wine.value = 0;
 shot.value = 0;
 
+radios[0].checked = true;
+femaleLabel.classList.add('checked');
+
+for(let i = 0; i < radios.length; i++) {
+  radios[i].onchange = () => {
+    if(radios[0].checked) {
+      femaleLabel.classList.add('checked');
+      maleLabel.classList.remove('checked');
+    } else {
+      femaleLabel.classList.remove('checked');
+      maleLabel.classList.add('checked');
+    }
+  };
+};
+
 while(timer <= 10) {
   var HTMLString = "";
-  HTMLString += '<option value="' + timer + '">' + timer + ' hour(s)</option>';
+  HTMLString += '<option class="options" value="' + timer + '">' + timer + ' hour(s)</option>';
   time.insertAdjacentHTML('beforeend', HTMLString);
   timer += 0.5;
+};
+
+plusbeer.onclick = () => {
+  beer.value++;
+};
+
+minusbeer.onclick = () => {
+  if(beer.value != 0) {
+    beer.value--;
+  }
+};
+
+pluswine.onclick = () => {
+  wine.value++;
+};
+
+minuswine.onclick = () => {
+  if(wine.value != 0) {
+    wine.value--;
+  }
+};
+
+plusshot.onclick = () => {
+  shot.value++;
+};
+
+minusshot.onclick = () => {
+  if(shot.value != 0) {
+    shot.value--;
+  }
+};
+
+const checkRequiredFields = () => {
+  if(isNaN(parseInt(kg.value))) {
+    alert('Please set weight!');
+    return false;
+  }
+  return true;
 };
 
 beer.onchange = () => {
