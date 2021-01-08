918a123e8f484762a5cbbe8beb8ab7d8dc80bdb4
GYARMATI ÁDÁM <gyarmatiaadaam@gmail.com>
2021-01-08 15:16:27 +0100

    change of port reupload Procfile

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.use('/static', express.static('static'));
@@ -10,5 +10,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log('Listening to port');
+    console.log('Our app is running!');
 });
